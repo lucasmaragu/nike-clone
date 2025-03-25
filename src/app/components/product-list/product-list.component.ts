70efebf5709a48574409eb1e355e2d59df5f14c4
Lucas Martinez <lucas.martinez@ad.avancemgrup.com>
2025-03-25 10:27:45 +0100

    feat: success modal on add product to cart

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -3,22 +3,32 @@ import { CommonModule } from "@angular/common";
 import { Product } from "../../models/product";
 import { ProductService } from "../../services/product/product.service";
 import { CartService } from "../../services/cart/cart.service";
+import { SuccessModalComponent } from "../success-modal/success-modal.component";
 
 @Component({
   selector: "app-product-list",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SuccessModalComponent],
   templateUrl: "./product-list.component.html",
 })
 export class ProductListComponent implements OnInit {
+  modalMessage: string = "";
+  showModal = false;
+
   constructor(public productService: ProductService, public  cartService: CartService) {}
 
   ngOnInit() {
-    // Asegurarnos de que se cargan los productos
     this.productService.fetchProducts()
   }
 
   addToCart(product: Product) {
+    try {
     this.cartService.addToCart(product)
+  } catch (err: any) {
+    this.modalMessage = err.message;
+    this.showModal = true;
   }
+  this.showModal = true;
+  this.modalMessage = "Producto agregado al carrito";
+}
 }
